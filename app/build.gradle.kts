714d07b56322812f7fcd16679e68f09d6a22a95d
xuechengkun7-ui <xuechengkun7@gmail.com>
2026-09-22 21:58:20 +0800

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.xuechengkun.truckleadradar"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.xuechengkun.truckleadradar"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+}
